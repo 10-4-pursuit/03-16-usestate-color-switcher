92c8714053feb6fdc03891302fa41b3f56aed14f
dttrue <danieltorres@pursuit.org>
2023-12-08 00:18:37 -0500

    Daniels add ColorSwitch component

---
+++ jsx-basic-react-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ColorSwitcher from './ColorSwitcher';
+
+function App() {
+  return (
+    <div className="App">
+     <ColorSwitcher />
+    </div>
+  );
+}
+
+export default App;
