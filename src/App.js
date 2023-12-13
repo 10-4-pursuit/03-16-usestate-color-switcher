cfbcf5659945e7bd12cbc2ce66f833b660ea0fa7
Angl99 <anggelplasencia@pursuit.org>
2023-12-12 21:34:43 -0500

    finished exercise

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ColorSwitcher from './ColorSwitcher';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ColorSwitcher />
+      </header>
+    </div>
+  );
+}
+
+export default App;
